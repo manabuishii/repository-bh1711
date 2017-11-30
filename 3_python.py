09ef46527891ff9c8c87367590bf4971482ddd5b
Manabu ISHII <manabu.ishii.rb@gmail.com>
2017-11-30 10:58:04 +0900

    Create 3_python.py

---
+++ 3_python.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- Coding: utf-8 -*-
+#
+# プログラムの修正もできます
+#
+
+def test1():
+  print("ここはtest1")
+  
+def test2():
+  print("ここはes2")
+
+def test3():
+  for i in range(10):
+    print("ループ %d" % i)
+
+
+print("Hello world!!")
+test1()
+test2()
+test3()
